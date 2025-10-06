861bf845948f072568988726e838e45a7260f1e1
GuilhermeHalterNunes <guilherme_h_nunes@estudante.sesisenai.org.br>
2025-10-06 17:54:31 -0300

    OTR-49 Configuração Inicial do Banco de Dados

--- database/otimizetour.sql
+++ database/otimizetour.sql
@@ -4,6 +4,49 @@ USE otimizetour;
 CREATE TABLE usuarios (
     id INT PRIMARY KEY AUTO_INCREMENT, 
     nome VARCHAR(50) NOT NULL, 
-    email VARCHAR(30) NOT NULL, 
+    email VARCHAR(30) NOT NULL UNIQUE, 
     senhaHash VARCHAR(100) NOT NULL 
+);
+
+CREATE TABLE Categorias (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE Roteiros (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    destino VARCHAR(100) NOT NULL,
+    dataInicio DATE NOT NULL,
+    dataFim DATE NOT NULL,
+    custoTotal DECIMAL(10, 2) NOT NULL,
+    usuario_id INT,
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
+);
+
+CREATE TABLE PontosInteresse (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    descricao TEXT,
+    avaliacaoMedia DECIMAL(2, 1),
+    categoria_id INT,
+    FOREIGN KEY (categoria_id) REFERENCES Categorias(id)
+);
+
+CREATE TABLE Roteiro_PontosInteresse (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    roteiro_id INT,
+    pontoInteresse_id INT,
+    FOREIGN KEY (roteiro_id) REFERENCES Roteiros(id),
+    FOREIGN KEY (pontoInteresse_id) REFERENCES PontosInteresse(id)
+);
+
+CREATE TABLE Avaliacoes (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nota INT CHECK (nota >= 1 AND nota <= 5),
+    comentario TEXT,
+    dataComentario DATE NOT NULL,
+    usuario_id INT,
+    roteiro_id INT,
+    FOREIGN KEY (roteiro_id) REFERENCES Roteiros(id),
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id),
 );
