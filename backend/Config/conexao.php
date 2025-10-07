621270d1aa82553d84e092529891d5fe7cdb92dc
GuilhermeHalterNunes <guilherme_h_nunes@estudante.sesisenai.org.br>
2025-10-07 16:38:22 -0300

    OTR-50 Implementação da Conexão Backend-Database

--- backend/Config/conexao.php
+++ backend/Config/conexao.php
@@ -7,7 +7,9 @@ $password = '';
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Conexão bem-sucedida!";
 } catch (PDOException $e) {
     die("Erro na conexão com o banco de dados: " . $e->getMessage());
+    echo "Erro de conexão com o banco de dados.";
 }
 ?>
